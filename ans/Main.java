da1de01dc0bdcd40e02f66b5395042ca84490005
amansingh057 <amansingh.95409540@gmail.com>
2023-04-17 15:33:14 +0530

    alter

--- ans/Main.java
+++ ans/Main.java
@@ -1,29 +1,18 @@
 import java.io.*;
 import java.util.*;
-public class Main
-{
-    // public static Integer fun(Integer a,Integer b){
-    //     if(2>b && (b^a)<(2-b)){
-    //         a=3+a+a;
-    //         a=a+2;
-    //         return a+fun(a,b);
-    //     }else{
-    //         return b-1;
-    //     }
-    // }
-    // public static Integer fun(Integer a,Integer b){
-    //     if(b<a && (b-a)<(a-b)){
-    //         a=2+2+b;
-    //         a=a+1;
-    //         a=b+1;
-    //         return a-fun(b,b);
-    //     }else if(a==2+2+a){
-    //         return a+b-1;
-    //     }
-    //     return 0;
-    // }
-    
-	public static void main(String[] args) {
-		// System.out.println(fun(7,2));
-	}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> numbers = new ArrayList<Integer>();
+        int num = 0;
+        System.out.println("Start entering the numbers in array :");
+        while (scanner.hasNextInt()) {
+            System.out.println("Insert + and press enter to exit");
+            num = scanner.nextInt();
+            numbers.add(num);
+        }
+        System.out.println(numbers);
+
+    }
 }
